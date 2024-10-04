d3d0a3846b63852a76e46311939f57a28cf66382
ninica0 <santosnico048@gmail.com>
2024-10-04 15:10:30 -0300

    feat: Sistema que mostra números de 15 a 200 ao quadrado

---
+++ Quadrado.java
@@ -0,0 +1,12 @@
+public class Quadrado {
+
+	public static void main(String[] args) {
+		int i, resultado;
+		
+		for(i=15; i<=200; i ++) {
+			resultado = i * i;
+			System.out.println(i + " Este número ao quadrado é: : "+ resultado);
+	}
+
+}
+}
